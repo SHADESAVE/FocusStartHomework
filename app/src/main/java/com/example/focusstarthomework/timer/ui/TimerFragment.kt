a788159beb878cdf2ab2090e85b6bbb26e9d9c32
Alexander Deev <shadesave1999@gmail.com>
2020-11-08 23:00:10 +0700

    change timer & coroutine worker

--- app/src/main/java/com/example/focusstarthomework/timer/ui/TimerFragment.kt
+++ app/src/main/java/com/example/focusstarthomework/timer/ui/TimerFragment.kt
@@ -1,33 +1,34 @@
 package com.example.focusstarthomework.timer.ui
 
+import android.content.Context
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import androidx.work.WorkRequest
+import androidx.lifecycle.Observer
+import androidx.work.*
 import com.example.focusstarthomework.R
 import com.example.focusstarthomework.utils.checkBattery
+import com.example.focusstarthomework.utils.makeStatusNotification
 import com.example.focusstarthomework.utils.toTimeFormatString
 import kotlinx.android.synthetic.main.fragment_timer.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import java.util.concurrent.TimeUnit
 
 const val MIN_TIME = 0
 const val MAX_TIME = 60
-const val TICK_ERROR = 999L
+const val TIMER_TIME = "TIMER_TIME"
+const val TIMER_TAG = "TIMER_TAG"
 
 class TimerFragment : Fragment() {
 
+    private val workManager by lazy { WorkManager.getInstance(requireContext()) }
+    private var timerJob: Job? = null
+
     private var overallTimeInMilliseconds = 0L
     private var timerState = TimerState.STOP
-    private var timer: CountDownTimer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,7 +37,7 @@ class TimerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        workManager.cancelAllWorkByTag(TIMER_TAG)
         checkBattery(requireContext())
 
         setupNumberPickers()
@@ -46,42 +47,42 @@ class TimerFragment : Fragment() {
 
     override fun onStop() {
         super.onStop()
+        startTimerWorker()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        workManager.cancelAllWorkByTag(TIMER_TAG)
+    }
+
+    private fun startTimerWorker() {
         if (timerState == TimerState.START || timerState == TimerState.RESUME) {
-            val uploadWorkRequest: WorkRequest =
-                OneTimeWorkRequestBuilder<TimerWorker>().setInitialDelay(overallTimeInMilliseconds, TimeUnit.MILLISECONDS).build()
-            WorkManager.getInstance(requireContext()).enqueue(uploadWorkRequest)
-
+            workManager.enqueue(
+                OneTimeWorkRequest.Builder(TimerWorker::class.java)
+//                    .setInitialDelay(overallTimeInMilliseconds, TimeUnit.MILLISECONDS)
+                    .setInputData(
+                        Data.Builder().putLong(TIMER_TIME, overallTimeInMilliseconds).build()
+                    )
+                    .addTag(TIMER_TAG)
+                    .build()
+            )
         }
     }
 
-    private fun setupRefreshButton() {
-        refresh_image_button.setOnClickListener {
-            it.visibility = View.GONE
-            timer_finish_text.visibility = View.GONE
-
-            play_button_switcher.visibility = View.VISIBLE
-            hours_picker.visibility = View.VISIBLE
-            minutes_picker.visibility = View.VISIBLE
-            seconds_picker.visibility = View.VISIBLE
-
-            if (play_button_switcher.currentView !== play_image)
-                play_button_switcher.showNext()
-
-            timer_text.text = getString(R.string.default_time)
-
-            hours_picker.value = MIN_TIME
-            minutes_picker.value = MIN_TIME
-            seconds_picker.value = MIN_TIME
-
-            changeState(TimerState.STOP)
-        }
+    private fun setupNumberPickers() {
+        hours_picker.minValue = MIN_TIME
+        hours_picker.maxValue = MAX_TIME
+        minutes_picker.minValue = MIN_TIME
+        minutes_picker.maxValue = MAX_TIME
+        seconds_picker.minValue = MIN_TIME
+        seconds_picker.maxValue = MAX_TIME
     }
 
     private fun setupPlayButtonSwitcher() {
         play_button_switcher.setOnClickListener {
             if (hours_picker.value > 0 || minutes_picker.value > 0 || seconds_picker.value > 0) {
                 refresh_image_button.visibility = View.VISIBLE
+
                 hours_picker.visibility = View.GONE
                 minutes_picker.visibility = View.GONE
                 seconds_picker.visibility = View.GONE
@@ -99,65 +100,77 @@ class TimerFragment : Fragment() {
         }
     }
 
+    private fun setupRefreshButton() {
+        refresh_image_button.setOnClickListener {
+            it.visibility = View.GONE
+
+            hours_picker.visibility = View.VISIBLE
+            minutes_picker.visibility = View.VISIBLE
+            seconds_picker.visibility = View.VISIBLE
+
+            if (play_button_switcher.currentView !== play_image)
+                play_button_switcher.showNext()
+
+            timer_text.text = getString(R.string.default_time)
+
+            hours_picker.value = MIN_TIME
+            minutes_picker.value = MIN_TIME
+            seconds_picker.value = MIN_TIME
+
+            changeState(TimerState.STOP)
+
+            timer_finish_text.visibility = View.GONE
+            play_button_switcher.visibility = View.VISIBLE
+        }
+    }
+
+
     private fun changeState(timerState: TimerState) {
         this.timerState = timerState
 
         when (this.timerState) {
             TimerState.START -> {
                 overallTimeInMilliseconds = getTimeInMilliseconds()
-                startTimer()
+                startTimerJob()
             }
-            TimerState.PAUSE -> timer?.cancel()
-            TimerState.RESUME -> {
-                startTimer()
+            TimerState.PAUSE -> timerJob?.cancel()
+            TimerState.RESUME -> startTimerJob()
+            TimerState.STOP -> {
+                timerJob?.cancel()
+                updateViewAfterTimerStopped()
             }
-            TimerState.STOP -> timer?.cancel()
         }
     }
 
-    private fun startTimer() {
-        timer = initTimer(overallTimeInMilliseconds)
-        GlobalScope.launch {
-            delay(500L)
-            Log.d("timerTimeInS", overallTimeInMilliseconds.toString())
-            timer?.start()
-        }
-    }
-
-    private fun updateTimerText() {
+    private fun updateViewAfterTimerStopped() {
+        overallTimeInMilliseconds = 0L
         timer_text.text = overallTimeInMilliseconds.toTimeFormatString()
+        timer_finish_text.visibility = View.VISIBLE
+        play_button_switcher.visibility = View.GONE
     }
 
-    private fun initTimer(timeInMilliSeconds: Long): CountDownTimer =
-        object : CountDownTimer(timeInMilliSeconds, 1000) {
 
-            override fun onFinish() {
-                overallTimeInMilliseconds = 0
-                timer_finish_text.visibility = View.VISIBLE
-                play_button_switcher.visibility = View.GONE
+    private fun startTimerJob() {
+        timerJob = CoroutineScope(Dispatchers.Main).launch {
+            try {
+                while (overallTimeInMilliseconds > 0) {
+                    timer_text.text = overallTimeInMilliseconds.toTimeFormatString()
+                    delay(1000L)
+                    overallTimeInMilliseconds -= 1000L
+                }
+                changeState(TimerState.STOP)
+                makeStatusNotification("Время истекло", requireContext())
 
-                val uploadWorkRequest: WorkRequest =
-                    OneTimeWorkRequestBuilder<TimerWorker>().build()
-                WorkManager.getInstance(requireContext()).enqueue(uploadWorkRequest)
-
-            }
-
-            override fun onTick(timeInMilliSeconds: Long) {
-                overallTimeInMilliseconds = timeInMilliSeconds
-                Log.d("timerTick", timeInMilliSeconds.toString())
-                updateTimerText()
+            } catch (e: Throwable) {
+                Log.d("timeJobCatch", e.toString())
             }
         }
+    }
 
-    private fun getTimeInMilliseconds(): Long =
-        (hours_picker.value * 360 + minutes_picker.value * 60 + seconds_picker.value) * 1000.toLong() + TICK_ERROR
-
-    private fun setupNumberPickers() {
-        hours_picker.minValue = MIN_TIME
-        hours_picker.maxValue = MAX_TIME
-        minutes_picker.minValue = MIN_TIME
-        minutes_picker.maxValue = MAX_TIME
-        seconds_picker.minValue = MIN_TIME
-        seconds_picker.maxValue = MAX_TIME
+    private fun getTimeInMilliseconds(): Long {
+        val millisecondsInHour = 3600000L
+        val millisecondsInMinute = 60000L
+        val millisecondsInSecond = 1000L
+        return hours_picker.value * millisecondsInHour + minutes_picker.value * millisecondsInMinute + seconds_picker.value * millisecondsInSecond
     }
 }
--- app/src/main/java/com/example/focusstarthomework/timer/ui/TimerWorker.kt
+++ app/src/main/java/com/example/focusstarthomework/timer/ui/TimerWorker.kt
@@ -5,19 +5,27 @@ import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.example.focusstarthomework.utils.makeStatusNotification
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 
 class TimerWorker(context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
-
         val appContext = applicationContext
 
-        return try {
-            makeStatusNotification("Время истекло", appContext)
+
+        return coroutineScope {
+            val job = async {
+                var time = inputData.getLong(TIMER_TIME, 0L)
+                while (time > 0) {
+                    delay(1000)
+                    time -= 1000
+                }
+                makeStatusNotification("Время истекло воркер $time", appContext)
+            }
+            job.await()
             Result.success()
-        } catch (error: Throwable) {
-            Log.e("TimerWorkerFailure:", error.toString())
-            Result.failure()
         }
     }
 }
